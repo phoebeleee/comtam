4c2614b5374c9bcd0e63d8b3d37ff8f7e3767065
phoebeleee <phoebelee1027@gmail.com>
2026-05-15 14:14:33 +0900

    data directory

--- Ex_ch11_cars.py
+++ Ex_ch11_cars.py
@@ -221,7 +221,7 @@ def cars_predict(df):
     st.success(f"예상 연비: **{mpg_pred:.2f} mpg** 🚘")
 
 def load_data():
-    df = pd.read_csv("./data/cars.csv")
+    df = pd.read_csv("./cars.csv")
     return df
 
 def main():
